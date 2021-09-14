23e3980f54384b28fed0aa43b56f5d8e5fcb34ea
ming.wang4 <ming.wang4@envision-digital.com>
2021-09-14 21:11:13 +0800

    init nacos config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-boot-hello-nacos
+
+https://nacos.io/zh-cn/docs/quick-start-spring-boot.html
---
+++ src/test/java/com/example/springboothellonacos/SpringBootHelloNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothellonacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloNacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
